d48015892f15d57c291899cc6b637cde06b079f8
Guardianz89 <125425071+Guardianz89@users.noreply.github.com>
2023-02-15 15:33:14 -0600

    Create GroupAdd.ps1
    This script is useful for adding users to a group based off of a attirbute in AD

---
+++ GroupAdd.ps1
@@ -0,0 +1,23 @@
+#This script is useful for adding users to a group based off of a attirbute in AD
+
+# Set the name of the AD attribute to check and the value it should be
+$attributeName = "ATTIBUTENAME"
+$attributeValue = "VALUE"
+
+# Set the name of the group to add the users to
+$groupName = "GROUP_NAME"
+
+#Comment or uncomment which option you'd like to use or make your own!
+# Get all users with the specified attribute value
+$users = Get-ADUser -Filter '$attributeName -eq $attributeValue'
+
+# Get all users within a specific OU and with a specific Attribute
+#$users = Get-ADUser -SearchBase "OU=Subs,OU=Users,OU=NISD,DC=nisdtx,DC=org" -Filter '$attributeName -eq $attributeValue'
+
+
+
+# Add each user to the specified group
+foreach ($user in $users) {
+    Add-ADGroupMember -Identity $groupName -Members $user
+
+}
