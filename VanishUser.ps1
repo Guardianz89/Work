77d70200b67dbbaf72b16930994316f55a542d4d
Guardianz89 <125425071+Guardianz89@users.noreply.github.com>
2023-10-06 15:23:32 -0500

    Update VanishUser.ps1

--- VanishUser.ps1
+++ VanishUser.ps1
@@ -1,46 +1,13 @@
-$EIDCSV="C:\Scripts\Vanishing\UnVanishUsers.csv"
+$EIDCSV="C:\Scripts\Vanishing\VanishUsers.csv"
 $EIDS=import-csv $EIDCSV
 
 #Variable
-#$EID="999999"
-ForEach ($EID in $EIDS)
-{
-$EID=$EID.EID
-$seek=""
+#$EID="7654321"
+ForEach ($EID in $EIDS) {
+    $user = get-aduser -server padawan.jedi.org  -Filter 'employeeID -like $EID'
+    $UPN=(Get-ADUser -Server adawan.jedi.org -Filter 'employeeID -like $EID').UserPrincipalName
+    Remove-ADUser -Server adawan.jedi.org -Identity $user
 
-$seek=$(try {Get-ADUser -Server padawans.jedi.org -filter 'employeeID -like $EID'} catch {$null})
-
-#Get ObjectGUID
-if($seek -ne $null){ 
-  
-
-$GUID=(Get-ADUser -Server padawans.jedi.org -filter 'employeeID -like $EID').ObjectGUID
-$UPN=(Get-ADUser -Server padawans.jedi.org -filter 'employeeID -like $EID').UserPrincipalName
-$SAM=(Get-ADUser -Server padawans.jedi.org -filter 'employeeID -like $EID').sAMAccountName
-
-$SAM2=$SAM+"2"
-
-$seek2=$(try {Get-ADUser -Server padawans.jedi.org -Identity $SAM2  } catch {$null})
-if($see2k -ne $null){
-  write-host $UPN has a 2 -ForegroundColor Yellow
-}
-
- # Write-Host $EID is $UPN  purging $GUID
-
-  #remove User
-  # Remove-ADUser -Server padawans.jedi.org -Identity $GUID
-
-  write-host Checking to restore $UPN
-   #Deleting User from Google
-   GAM UnDelete User $UPN
-
-}
-else{
- Write-Host $EID was not found
-
-}
-
-
-
-
+    #Deleting User from Google
+GAM Delete User $UPN
 }
